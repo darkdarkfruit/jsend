c19e75422b5a3933eff57541cbf9c8c8869ad4d8
darkdarkfruit <darkdarkfruit@gmail.com>
2011-11-12 16:02:54 +0800

    hardlink README to README.rst; add a .sh script for convinience making package;
    	modified:   .gitignore
    	new file:   README
    	modified:   README.rst
    	new file:   dist/python-jsend-0.1.7.tar.gz
    	modified:   jsend/__init__.py
    	new file:   make_package.sh

---
+++ make_package.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo '======= running: python setup.py sdist ========================='
+python setup.py sdist
+
+echo 
+
+#echo '======= running: python setup.py bdist --formats=gztar ========='
+#python setup.py bdist --formats=gztar 
